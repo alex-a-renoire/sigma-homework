93f034e2b3397070b6fad486a3a7ac5b43f41a05
Alex A. Renoire <aleksandra.alekseeva@sigma.software>
2021-08-30 19:08:14 +0300

    mongodb CRUD fully functional

--- pkg/storage/mongostorage/mongostorage.go
+++ pkg/storage/mongostorage/mongostorage.go
@@ -3,12 +3,10 @@ package mongostorage
 import (
 	"context"
 	"fmt"
-	"log"
 
 	"github.com/alex-a-renoire/sigma-homework/model"
 	"github.com/google/uuid"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -17,20 +15,16 @@ type MongoPersonStorage struct {
 	Client *mongo.Client
 }
 
+//TODO: ask how to close the connection?
 func New(addr string, user string, password string) (*MongoPersonStorage, error) {
-	//ctx, _ := context.WithTimeout(context.Background(), 10*time.Second) //TODO: ask what to do with cancel function?
-	log.Print("mongodb:" + addr)
 	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI("mongodb://"+user+":"+password+"@"+addr+"/persons"))
 	if err != nil {
 		return nil, fmt.Errorf("failed to open the db connection: %w", err)
 	}
-
-	//TODO: ask how to close the connection?
-
 	// Check the connection
 	err = client.Ping(context.TODO(), nil)
 	if err != nil {
-		return nil, fmt.Errorf("Conncetion is not yet established: %w", err)
+		return nil, fmt.Errorf("connection is not yet established: %w", err)
 	}
 	fmt.Println("Connected to MongoDB!")
 
@@ -62,35 +56,48 @@ func (mg *MongoPersonStorage) GetPerson(id uuid.UUID) (model.Person, error) {
 }
 
 func (mg *MongoPersonStorage) GetAllPersons() ([]model.Person, error) {
-	return nil, nil
+	collection := mg.Client.Database("persons").Collection("persons")
+
+	cursor, err := collection.Find(context.Background(), bson.M{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to get all persons from db: %w", err)
+	}
+
+	persons := []model.Person{}
+
+	for cursor.Next(context.Background()) {
+		p := model.Person{}
+		err := cursor.Decode(&p)
+		if err != nil {
+			return nil, fmt.Errorf("failed to marshal to binary all persons from db: %w", err)
+		}
+		persons = append(persons, p)
+	}
+	return persons, nil
 }
 
 func (mg *MongoPersonStorage) UpdatePerson(id uuid.UUID, person model.Person) error {
 	collection := mg.Client.Database("persons").Collection("persons")
 
-	_id, err := primitive.ObjectIDFromHex(id.String())
+	filter := bson.D{{Key: "_id", Value: id}}
+	update := bson.D{{Key: "$set",
+		Value: bson.D{
+			{Key: "name", Value: person.Name},
+		},
+	}}
+
+	_, err := collection.UpdateOne(context.TODO(), filter, update)
 	if err != nil {
 		return fmt.Errorf("failed to update person: %w", err)
 	}
-	filter := bson.D{{Key: "id", Value: _id}}
-	update := bson.D{{"$set", primitive.E{Key: "name", Value: person.Name}}}
-
-	collection.FindOneAndUpdate(context.TODO(), filter, update)
-
 	return nil
-	//TODO find how to check if updates were successfull
 }
 
 func (mg *MongoPersonStorage) DeletePerson(id uuid.UUID) error {
 	collection := mg.Client.Database("persons").Collection("persons")
 
-	_id, err := primitive.ObjectIDFromHex(id.String())
-	if err != nil {
-		return fmt.Errorf("failed to delete person: %w", err)
-	}
-
 	opts := options.Delete().SetCollation(&options.Collation{})
-	if _, err := collection.DeleteOne(context.TODO(), bson.D{{Key: "_id", Value: _id}}, opts); err != nil {
+	if _, err := collection.DeleteOne(context.TODO(), bson.D{{Key: "_id", Value: id}}, opts); err != nil {
 		return fmt.Errorf("failed to delete person: %w", err)
 	}
 	return nil
